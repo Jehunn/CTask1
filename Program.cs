8cf943d645c97992e8f27f77a9469fc9f9843cc3
Jehun <121166619+Jehunn@users.noreply.github.com>
2023-01-17 14:29:42 +0400

    C#

---
+++ Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //#region Verilmish a,b,c edeleri uzerinden en kichiyini tapan proqram
+            //int a = 41;
+            //int b = 7;
+            //int c = 12;
+            //if (a <= b && a <= c)
+            //{
+            //    Console.WriteLine(" En kicik eded " + a); 
+            //}
+            //else if (b <= a && b <= c)
+            //{
+            //    Console.WriteLine(" En kicik eded " + b);
+            //}         
+            //else
+            //{
+            //    Console.WriteLine(" En kicik eded " + c);
+            //}
+
+
+            //int[] array = { 13, 32, 21, 33, 48 };
+            //int sum = 0;
+            //int count = 0;
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    if (array[i] % 3 == 0)
+            //    {
+            //        sum += array[i];
+            //        count++;
+            //    }
+            //}
+            //double average = sum / count;
+            //Console.WriteLine(average);
+
+
+
+
+
+
+            //int[] arr = { 62, 12, 39, 66, 72 };
+            //int sum = 0;
+            //int count = 0;
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] % 3 == 0)
+            //    {
+            //        sum += arr[i];
+            //        count++;
+            //    }
+            //}
+
+            //double average = sum / count;
+            //Console.WriteLine(average);
+
+
+
+
+
+
+
+
+
+
+
+            //int[] numbers = { 233, 44, 8, 490, 120, 46, 8, 666 };
+            //int count = 0;
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] > 99 && numbers[i] < 1000)
+            //    {
+            //        count++;
+            //    }
+            //}
+
+            //Console.WriteLine(count);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
